e4d61a7cd3d0f892b3cfc138797f4df176079a35
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-16 18:14:39 +0100

    add for loop

---
+++ jsnack7/script.js
@@ -0,0 +1,9 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+let emptyArray = [i];   
+
+for(i = 0; i + 6; i < 7) {
+    let i = prompt("Inserisci un numero");
+} if (i % 2 == 1){
+    emptyArray.push(i);
+}
